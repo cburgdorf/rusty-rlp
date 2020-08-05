89c2ce47fdba0619f84eed1e8fc03432ee2a5af3
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-08-05 08:49:45 +0200

    Remove unused test

--- tests/core.rs
+++ tests/core.rs
@@ -48,11 +48,6 @@ mod tests {
     use super::*;
     use test::Bencher;
 
-    #[test]
-    fn it_works() {
-        assert_eq!(4, add_two(2));
-    }
-
     #[bench]
     fn bench_encode_u64(b: &mut Bencher) {
         b.iter(|| {
