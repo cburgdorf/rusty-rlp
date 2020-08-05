94aaa19001da3beef25504c23e30defccdf1d5a6
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-08-05 11:20:15 +0200

    Add tests for binary

---
+++ tests/binary.rs
@@ -0,0 +1,19 @@
+#![feature(test)]
+
+use rlp;
+use rstest::rstest;
+
+#[rstest(input, expected,
+    case(vec![101, 102], vec![101, 102]),
+    case(b"asdf".to_vec(), b"asdf".to_vec()),
+)]
+fn encode_bytes(input: Vec<u8>, expected: Vec<u8>) {
+    let mut stream = rlp::RlpStream::new();
+    // We need to feed the values one by one because adding continues u8 at once is treated
+    // as adding a string and hence result in a different encoding
+    for i in input {
+        stream.append(&i);
+    }
+
+    assert_eq!(stream.out(), expected);
+}
