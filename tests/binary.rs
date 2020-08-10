d52738e0bd1fab8554585be4e319f4a7ff5722c0
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-08-10 09:35:59 +0200

    Add append_raw test

--- tests/binary.rs
+++ tests/binary.rs
@@ -17,3 +17,16 @@ fn encode_bytes(input: Vec<u8>, expected: Vec<u8>) {
 
     assert_eq!(stream.out(), expected);
 }
+
+
+#[rstest(input, expected,
+    case(vec![101, 102], vec![101, 102]),
+    case(b"asdf".to_vec(), b"asdf".to_vec()),
+)]
+fn encode_bytes2(input: Vec<u8>, expected: Vec<u8>) {
+    let mut stream = rlp::RlpStream::new();
+    // Alternatively instead, we can use append_raw
+    stream.append_raw(&input, 1);
+
+    assert_eq!(stream.out(), expected);
+}
