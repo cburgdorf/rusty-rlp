7fd992c8a57be9241f0f08d4b33516293aa91ed6
Christoph Burgdorf <christoph.burgdorf@gmail.com>
2020-08-28 11:42:59 +0200

    Move errors out of lib

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,19 @@
-use crate::DecodingError;
+use pyo3::create_exception;
+use pyo3::exceptions::Exception;
 use pyo3::prelude::*;
 use rlp::PayloadInfo;
+
+create_exception!(rusty_rlp, EncodingError, Exception);
+create_exception!(rusty_rlp, DecodingError, Exception);
+
+// We can not implement From for rlp::DecoderError as it is in a foreign crate. Hence, we use
+// map_err and implement From on RlpDecoderError instead.
+pub struct RlpDecoderError(pub rlp::DecoderError);
+impl std::convert::From<RlpDecoderError> for PyErr {
+    fn from(err: RlpDecoderError) -> PyErr {
+        DecodingError::py_err(err.0.to_string())
+    }
+}
 
 pub fn construct_short_string_error<T>(val: &u8) -> Result<T, PyErr> {
     Err(DecodingError::py_err(format!(
